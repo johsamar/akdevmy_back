14d81c7af44541cfcd66d6e197d5ab292f77671f
Eider22 <eidercardona22@gmail.com>
2023-04-29 08:29:24 -0500

    The initial project with the initial required dependencies is created

---
+++ src/main/java/com/softlond/akdevmy/AkdevmyApplication.java
@@ -0,0 +1,13 @@
+package com.softlond.akdevmy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkdevmyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkdevmyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/softlond/akdevmy/AkdevmyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softlond.akdevmy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AkdevmyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
