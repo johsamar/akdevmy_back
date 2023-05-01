0b3813e4975b62dfe06c5f62aecf2c88fcc4dc8d
Eider22 <eidercardona22@gmail.com>
2023-04-30 19:40:54 -0500

    Correction to modules collection name

--- src/main/java/com/softlond/akdevmy/models/Module.java
+++ src/main/java/com/softlond/akdevmy/models/Module.java
@@ -7,7 +7,7 @@ import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection = "Modulos")
+@Document(collection = "Modules")
 public class Module {
 
 	@Id
