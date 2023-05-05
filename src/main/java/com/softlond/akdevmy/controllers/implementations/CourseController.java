2e44857a37022049b1103cfafe3d2880bf8e8cbf
GioRestrepo <gierestrepove@unal.edu.co>
2023-05-05 13:37:53 -0500

    feat: add CORS

--- src/main/java/com/softlond/akdevmy/controllers/implementations/CourseController.java
+++ src/main/java/com/softlond/akdevmy/controllers/implementations/CourseController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST})
 @RestController
 public class CourseController {
 
