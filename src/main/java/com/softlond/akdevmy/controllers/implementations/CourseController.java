bfbb0c8199d66a63b3adc872fd4cf67a6f1ac839
GioRestrepo <gierestrepove@unal.edu.co>
2023-05-09 16:05:25 -0500

    feat: Create delete course

--- src/main/java/com/softlond/akdevmy/controllers/implementations/CourseController.java
+++ src/main/java/com/softlond/akdevmy/controllers/implementations/CourseController.java
@@ -6,7 +6,6 @@ import com.softlond.akdevmy.models.Course;
 import com.softlond.akdevmy.responses.CustomResponse;
 import com.softlond.akdevmy.services.contracts.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
@@ -47,7 +46,7 @@ public class CourseController {
 
        return courseService.createCourse(course).map(c -> ResponseEntity.ok()
                         .header("Content-Type", "application/json; charset=UTF-8")
-                        .body(new CustomResponse<>("Ok: ",c)))
+                        .body(new CustomResponse<>("created successfully: ",c)))
                 .onErrorResume(err -> {
                     CustomResponse<Course> resp = new CustomResponse<>();
                     if (err instanceof CustomException) {
@@ -65,7 +64,7 @@ public class CourseController {
 
         return courseService.updateCourse(course,id).map(c -> ResponseEntity.ok()
                         .header("Content-Type", "application/json; charset=UTF-8")
-                        .body(new CustomResponse<>("Ok: ",c)))
+                        .body(new CustomResponse<>("updated successfully: ",c)))
                 .onErrorResume(err -> {
                     CustomResponse<Course> resp = new CustomResponse<>();
                     if (err instanceof CustomException) {
@@ -75,6 +74,13 @@ public class CourseController {
                     }
                     return Mono.just(new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR));
                 });
+
+    }
+
+    @DeleteMapping("/deleteCourse/{id}")
+    public Mono<Void> deleteCourse(@PathVariable String id){
+
+        return courseService.deleteCourse(id);
 
     }
 
